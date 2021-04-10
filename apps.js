b4fcd3d427ab86570c477aac0fdf002daabc489c
kinnitchi <79287925+Kinnitchi@users.noreply.github.com>
2021-04-09 22:03:28 -0300

    Add files via upload

---
+++ apps.js
@@ -0,0 +1,15 @@
+var n1 = document.getElementById('numero1')
+const s = (n1 == 2 * Math.PI * raio)
+
+function calcularCircunferencia(raio) {
+  return (n1 + 2 * Math.PI * raio);
+}
+console.log(calcularCircunferencia(numero1));
+
+
+const p = document.querySelector('#resposta');
+const txt = "A resposta é:"
+const p = txt.replace("A resposta é: ", s)
+
+
+
